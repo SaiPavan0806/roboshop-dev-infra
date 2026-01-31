6cd132f180bb2312883d9f47c59f857ce4770b9f
Bharath <bharathvaliveti06@gmail.com>
2026-01-31 14:15:01 -0800

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -1,8 +1,17 @@
-# resource "aws_security_group_rule" "frontend_frontend_alb" {
-# type = "ingress"
-# security_group_id = module.sg[9].sg_id #frotend SG ID
-# source_security_group_id = module.sg[11].sg_id # frontend ALB SG ID
-# from_port = 80
-# protocol = "tcp"
-# to_port = 80
-# }
+resource "aws_security_group_rule" "backend_alb_bastion" {
+type = "ingress"
+security_group_id = local.backend_alb_sg_id # backend ALB SG ID
+source_security_group_id = local.bastion_sg_id # bastion SG ID
+from_port = 80
+protocol = "tcp"
+to_port = 80
+}
+
+resource "aws_security_group_rule" "backend_laptop" {
+type = "ingress"
+security_group_id = local.bastion_sg_id # backend ALB SG ID
+cidr_blocks = ["0.0.0.0/0"] # laptop IP
+from_port = 22
+protocol = "tcp"
+to_port = 22
+}
